876f1f38d1a6e6ea81416c1d2af9676a544dfed3
ninjaneer324 <kakashiandflute@gmail.com>
2020-01-25 17:46:45 -0600

    chatterbox test 1

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
+
+#create chatbot
+englishBot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(englishBot)
+trainer.train("chatterbot.corpus.english") #train the chatter bot for english
 
 @app.route('/')
 def test():
     return render_template("index.html")
 
-
+@app.route("/get")
+#function for the bot response
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(englishBot.get_response(userText)
 
 if __name__ == "__main__":
     app.run(debug = True)
